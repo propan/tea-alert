9ae9d50adeffd6e8b096d26d1f0a00c8524700f3
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2017-07-13 19:22:24 +0300

    fix white2tea parser

--- src/tea_alert/parsers/white2tea.clj
+++ src/tea_alert/parsers/white2tea.clj
@@ -1,10 +1,10 @@
 (ns tea-alert.parsers.white2tea
   (:require [net.cgrand.enlive-html :as enlive]
-            [tea-alert.parsers.helpers :refer [extract-with]]))
+            [tea-alert.parsers.helpers :refer [extract-with ifnil make-absolute-url]]))
 
 (defn- extract-item
   [entry]
-  {:image (extract-with entry [:.product-image-wrapper :img] #(-> % :attrs :src))
+  {:image (extract-with entry [:.product-image-wrapper :img] #(-> % :attrs :src ifnil (make-absolute-url "http://white2tea.com")))
    :title (extract-with entry [:.product-title :a] #(-> % :content first))
    :url   (extract-with entry [:.product-title :a] #(-> % :attrs :href))
    :price (extract-with entry [:.price :.amount] #(enlive/text %))})
--- test/tea_alert/parsers/white2tea_test.clj
+++ test/tea_alert/parsers/white2tea_test.clj
@@ -11,67 +11,51 @@
 (deftest parse-test
   (testing "Correctly parses source page"
     (let [page (-> (absolute-path "white2tea.html") (java.io.File.) (enlive/html-resource) (parse))]
-      (is (= [{:image "http://white2tea.com/puer/wp-content/uploads/2017/01/Butter-Flower-Oolong-1-2-350x300.jpg"
-               :title "Butter Flower"
-               :url   "http://white2tea.com/product/butter-flower/"
-               :price "$24.50"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2017/01/milan-dancong-Oolong-1-350x300.jpg"
-               :title "Milan Dancong"
-               :url   "http://white2tea.com/product/milan-dancong/"
-               :price "$24.50"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2017/01/shuixian-Oolong-1-350x300.jpg"
-               :title "Shui Xian"
-               :url   "http://white2tea.com/product/shui-xian/"
-               :price "$14.50"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-nightlife-moonlight-white-tea-1-350x300.jpg"
-               :title "2016 Nightlife"
-               :url   "http://white2tea.com/product/2016-nightlife/"
-               :price "$4.70"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-post-truth-raw-Puerh-tea-1-350x300.jpg"
-               :title "2016 post truth"
-               :url   "http://white2tea.com/product/2016-post-truth/"
-               :price "$6.50"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-Chocobar-tea-sampleset-teatasting-1-350x300.jpg"
-               :title "ChocoBar Tasting Set"
-               :url   "http://white2tea.com/product/chocobar-tasting-set/"
-               :price "$55.00"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-Chocobar-tea-raw-Puer-c-350x300.jpg"
-               :title "2016 ChocoBrick Raw Puer"
-               :url   "http://white2tea.com/product/2016-chocobrick-raw-puer/"
-               :price "$14.00"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-Chocobar-tea-blacktea-c-350x300.jpg"
-               :title "2016 ChocoBrick Black Tea"
-               :url   "http://white2tea.com/product/2016-chocobrick-black-tea/"
-               :price "$15.00"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-Chocobar-tea-whitetea-c-350x300.jpg"
-               :title "2016 ChocoBrick White Tea"
-               :url   "http://white2tea.com/product/2016-chocobrick-white-tea/"
-               :price "$15.00"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-Chocobar-tea-ripe-Puer-tea-c-350x300.jpg"
-               :title "2016 ChocoBrick Ripe Puer"
-               :url   "http://white2tea.com/product/2016-chocobrick-ripe-puer/"
+      (is (= [{:image "http://white2tea.com/puer/wp-content/uploads/2017/07/2017-tuhao-as-fuck-raw-puerh-tea-1-350x300.jpg"
+               :title "2017 Tuhao af"
+               :url   "http://white2tea.com/product/2017-tuhao-as-fuck/"
+               :price "$19.50"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/07/2017-manichee-raw-puer-tea-1-350x300.jpg"
+               :title "2017 Manichee"
+               :url   "http://white2tea.com/product/2017-manichee/"
                :price "$11.00"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2013-baimudan-whitetea-350x300.jpg"
-               :title "2013 Baimudan"
-               :url   "http://white2tea.com/product/2013-baimudan/"
-               :price "$14.50"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2014-shoumei-350x300.jpg"
-               :title "2014 Shoumei"
-               :url   "http://white2tea.com/product/2014-shoumei/"
-               :price "$9.50"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2015-Channel-Orange-Ripe-Puerh-tea-350x300.jpg"
-               :title "2015 Channel Orange"
-               :url   "http://white2tea.com/product/2015-channel-orange/"
-               :price "$22.00"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-trap-bird-shou-Puerh-tea-1-350x300.jpg"
-               :title "2016 Trap Bird"
-               :url   "http://white2tea.com/product/2016-trap-bird/"
-               :price "$2.90"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-Sister-Brother-sampleset-tea-b-350x300.jpg"
-               :title "2016 Sister Brother"
-               :url   "http://white2tea.com/product/2016-sister-brother/"
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/07/2017-dangerous-messengers-puerh-tea-1-350x300.jpg"
+               :title "2017 Dangerous Messengers"
+               :url   "http://white2tea.com/product/2017-dangerous-messengers/"
+               :price "$8.50"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/07/2017-pussy-sheng-puer-tea-1-350x300.jpg"
+               :title "2017 Pussy"
+               :url   "http://white2tea.com/product/2017-pussy/"
+               :price "$12.50"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/07/2017-fuck-what-u-heard-tea-350x300.jpg"
+               :title "2017 fuck what u heard"
+               :url   "http://white2tea.com/product/2017-fuck-what-u-heard/"
+               :price "$27.00"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/07/2017-shes-not-me-puer-tea-350x300.jpg"
+               :title "2017 She’s Not Me"
+               :url   "http://white2tea.com/product/2017-shes-not-me/"
+               :price "$17.00"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/06/2017-U2CANHELP-charity-tea-1-350x300.jpg"
+               :title "2017 U 2 CAN HELP"
+               :url   "http://white2tea.com/product/2017-u-2-can-help/"
                :price "$49.00"}
-              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-Brown-Sugar-Ripe-Puerh-tea-b-350x300.jpg"
-               :title "2016 Brown Sugar"
-               :url   "http://white2tea.com/product/2016-brown-sugar/"
-               :price "$2.50"}] page)))))
+              {:image "http://white2tea.com/puer/wp-content/uploads/2016/11/2016-nightlife-moonlight-white-tea-1-350x300.jpg"
+               :title "2017 Nightlife"
+               :url   "http://white2tea.com/product/2017-nightlife/"
+               :price "$4.70"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/05/2002-Yiwu-Huangpian-Brick-350x300.jpg"
+               :title "2002 Yiwu Huangpian Raw Puer Brick"
+               :url   "http://white2tea.com/product/2002-yiwu-huangpian-raw-puer-brick/"
+               :price "$7.00"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/05/Ice-Green-Fujian-Greentea-tea-1-350x300.jpg"
+               :title "Ice Green Fujian"
+               :url   "http://white2tea.com/product/ice-green-fujian/"
+               :price "$8.75"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/03/2017-old-reliable-ripe-puer-tea-white2tea-1-350x300.jpg"
+               :title "2017 Old Reliable"
+               :url   "http://white2tea.com/product/2017-old-reliable/"
+               :price "$1.95"}
+              {:image "http://white2tea.com/puer/wp-content/uploads/2017/03/2017-grandpas-ripe-balls-puer-tea-350x300.jpg"
+               :title "2017 Grandpa’s"
+               :url   "http://white2tea.com/product/2017-grandpas/"
+               :price "$0.75"}] page)))))
