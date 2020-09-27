821d7249f7e1a95a69d58fb1bafa33a164cf3da3
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2020-09-27 10:58:29 +0300

    update FarmerLeaf parser

--- src/tea_alert/parsers/farmer_leaf.clj
+++ src/tea_alert/parsers/farmer_leaf.clj
@@ -2,14 +2,16 @@
   (:require [net.cgrand.enlive-html :as enlive]
             [clojure.string :as s]
             [tea-alert.parsers.helpers :refer [extract-with ifnil make-absolute-url]]))
+
 (defn- extract-item 
   [entry]
   {:image (extract-with entry [:.product_image :img] #(-> % :attrs :src ifnil (s/replace #"//" "https://")))
-   :title (extract-with entry [(enlive/attr= :itemprop "name")] #(-> % :content first))
-   :url   (extract-with entry [(enlive/attr= :itemprop "url")] #(-> % :attrs :href ifnil (make-absolute-url "https://www.farmer-leaf.com")))
-   :price "-"})
+   :title (extract-with entry [:.title] #(-> % :content first))
+   :url   (extract-with entry [:a] #(-> % :attrs :href ifnil (make-absolute-url "https://www.farmer-leaf.com")))
+   :price (extract-with entry [:.money :span] #(-> % :content first ifnil))})
 
 (defn parse
   [page]
-  (->> (enlive/select page [(enlive/attr= :itemprop "itemListElement")])
+  (->> (enlive/select page [:.products :div.thumbnail])
        (map extract-item)))
+
--- test/tea_alert/parsers/farmer_leaf_test.clj
+++ test/tea_alert/parsers/farmer_leaf_test.clj
@@ -11,23 +11,48 @@
 (deftest parse-test
   (testing "Correctly parses source page"
     (let [page (-> (absolute-path "farmer-leaf.html") (java.io.File.) (enlive/html-resource) (parse))]
-      (is (= [{:image "https://cdn.shopify.com/s/files/1/1318/5761/products/MG_3891_grande.JPG?v=1534420512"
-               :title "Spring 2018 Purple Black Tea"
-               :url   "https://www.farmer-leaf.com/collections/yunnan-black-tea/products/spring-2018-purple-black-tea"
-               :price "-"}
-              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/black_wok_1_grande.jpg?v=1525802334"
-               :title "Spring 2018 Jingmai Black Light Roast"
-               :url   "https://www.farmer-leaf.com/collections/yunnan-black-tea/products/spring-2018-jingmai-black-light-roast"
-               :price "-"}
-              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/black_short_ox_1_grande.jpg?v=1525798906"
-               :title "Spring 2018 Jingmai Sun-Dried Black -- Short oxidation"
-               :url   "https://www.farmer-leaf.com/collections/yunnan-black-tea/products/spring-2018-jingmai-sun-dried-black-short-oxidation"
-               :price "-"}
-              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/black_long_ox_3_grande.jpg?v=1525797932"
-               :title "Spring 2018 Sun-Dried Black -- Long Oxidation"
-               :url   "https://www.farmer-leaf.com/collections/yunnan-black-tea/products/spring-2018-sun-dried-black-long-oxidation"
-               :price "-"}
-              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/jingmai_black_f7ba76b0-4552-44ab-8fd8-0dde49d7b4ca_grande.jpg?v=1478726895"
-               :title "Autumn 2017 Dianhong Jingmai Black Tea"
-               :url   "https://www.farmer-leaf.com/collections/yunnan-black-tea/products/jingmai-dianhong-black-tea"
-               :price "-"}] page)))))
+      (is (= [{:image "https://cdn.shopify.com/s/files/1/1318/5761/products/summer2020pu-erhtea-8_50x.jpg?v=1597582542",
+               :title "Spring 2020 Lao Wu Shan",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/spring-2020-lao-wu-shan",
+               :price "$7 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/laoswrapper-1_50x.jpg?v=1597579257",
+               :title "Spring 2020 Huey Wa",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/spring-2020-huey-wa",
+               :price "$9 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/JingmaiMiyuncake_50x.jpg?v=1591715098",
+               :title "Jingmai Miyun",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/jingmai-miyun-1",
+               :price "$6 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/BulangMengnoycake_50x.jpg?v=1591767409",
+               :title "Meng Noy",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/bulang-meng-noy",
+               :price "$6 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/LafuGushucake_50x.jpg?v=1591771350",
+               :title "Lafu",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/lafu",
+               :price ""}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/NannuoGushucake_50x.jpg?v=1591701120",
+               :title "Nannuo",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/nannuo",
+               :price "$13 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/jingmaigulanwrappedcake_50x.jpg?v=1591596640",
+               :title "Jingmai Gulan",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/jingmai-gulan-1",
+               :price "$19 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/MG_3001_50x.jpg?v=1579001070",
+               :title "Spring 2018 Ao Ne Me Neglected Gardens",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/spring-2018-ao-ne-me-neglected-gardens",
+               :price "$7 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/20190730145026_50x.jpg?v=1569283648",
+               :title "Spring 2019 Mangjing",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/spring-2019-mangjing",
+               :price "$13 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/0014_1b9a1adf-143a-4b87-b7c2-5eab0bd443fe_50x.jpg?v=1569278122",
+               :title "Spring 2019 Jingmai Miyun",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/spring-2019-jingmai-miyun",
+               :price "$6 USD"}
+              {:image "https://cdn.shopify.com/s/files/1/1318/5761/products/miyun_1_50x.jpg?v=1569100771",
+               :title "Spring 2018 Jingmai Miyun",
+               :url   "https://www.farmer-leaf.com/collections/yunnan-pu-erh-tea/products/spring-2018-jingmai-miyun",
+               :price "$7 USD"}] page)))))
+
